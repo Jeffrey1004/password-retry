3d9dd119b87f73ce3497e8684b5c8ced91e42984
Jeffrey <jeffrey02208@gmail.com>
2020-02-03 19:56:04 +0800

    add new file

--- password-retry.py
+++ password-retry.py
@@ -1,10 +1,12 @@
 password = 'a123456'
 i = 3
 while i > 0:
+	i = i - 1
 	pwd = input('please enter the password: ')
 	if pwd == password:
 		print('login successful')
 		break
 	else:
-		i = i - 1
-		print('wrong password ! still have', i , 'more chance')
+		print('wrong password !')
+		if i > 0:
+			print('still have', i , 'more chance')
